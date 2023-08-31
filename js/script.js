c3858f46e20be6320c7abda3a1f712e64ce090d4
Sirvau <sirols50984@stud.noroff.no>
2023-08-31 14:10:07 +0200

    called api, displayed products on sortiment page

--- js/script.js
+++ js/script.js
@@ -14,14 +14,11 @@
 //</div>
 
 
-
-
 //Hardcoded HTML product page: sizes 
 //<div class="size">
 //<input type="radio" name="size" value="xs" id="xs" />
 //<label for="xs">XS</label>
 //</div>
-
 
 //Base URL API: https://api.noroff.dev/api/v1/ 
 
@@ -30,15 +27,59 @@
 
 //API call function: all products
 
-async function getProducts(){
-  const response = await fetch("https://api.noroff.dev/api/v1/rainy-days");
-  const results = await response.json();
-  console.log(results); //I manage to console.log the results, but not return the results. I get 404 not found. 
+//async function getProducts(){
+  //const response = await fetch("https://api.noroff.dev/api/v1/rainy-days");
+  //const results = await response.json();
+  //console.log(results); //I manage to console.log the results, but not return the results. I get 404 not found. 
+//}
+
+//getProducts();
+
+const apiUrl = "https://api.noroff.dev/api/v1/rainy-days";
+
+const getJacketInfo = document.querySelectorAll(".products");
+
+async function getProducts() {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+    return result;
 }
 
-getProducts();
+async function displayJackets() {
+    const jackets = await getProducts();
+    const jacketsContainer = document.getElementById("jackets_container");
+    
+    for (i = 0; i < jackets.length; i++) {
+        const jacket = jackets[i];
+        //console.log(jacket);
 
+        if(jacket.onSale){
+          const jacketDiv = document.createElement("div");
+          jacketDiv.classList.add("product");
+          console.log(jacketDiv.outerHTML);
 
+          const image = document.createElement("img");
+          image.src = jacket.image;
+          image.alt = jacket.description;
+
+          const products = document.createElement("p");
+          products.classList.add("products");
+          products.innerHTML = `${jacket.title} <span class="jacketSale">${jacket.price}</span> $${jacket.discountedPrice}`;
+
+          const button = document.createElement("a");
+          button.href ="#";
+          button.classList.add("button_small")
+          button.textContent = "View";
+
+          jacketsContainer.appendChild(image);
+          jacketsContainer.appendChild(products);
+          jacketsContainer.appendChild(button);
+         
+        }
+    }
+    }
+
+displayJackets();
 
 
 
